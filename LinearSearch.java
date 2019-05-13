a69123343124ed116bafea7d5f3e2d0edfdff6e4
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 14:13:43 +0530

    Add.list of numbers

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class LinearSearch {
     public static void main(String[] args) {
         int input=Integer.parseInt(args[0]);
+        Integer searchIn[]={1,33,5,11,89,3,22,15,19,29};
     }
 }
