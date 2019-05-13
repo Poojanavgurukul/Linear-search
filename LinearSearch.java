3bbd2c023953c457df21d021d69de206660fecfb
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 14:18:19 +0530

    Add.validate and search number in the list

--- LinearSearch.java
+++ LinearSearch.java
@@ -3,5 +3,12 @@ public class LinearSearch {
     public static void main(String[] args) {
         int input=Integer.parseInt(args[0]);
         Integer searchIn[]={1,33,5,11,89,3,22,15,19,29};
+        for (int i = 0; i <searchIn.length ; i++) {
+            if(searchIn[i]==input){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println("Number is not there");
     }
 }
